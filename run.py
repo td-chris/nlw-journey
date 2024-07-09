04679b319ae30ccf1e873982a9a714d42ce45636
Christian Todesquine Dias <todesquinechristian@gmail.com>
2024-07-09 15:20:55 -0300

    FEAT: implementando controllers das trips

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from src.main.server.server import app
+# importacao de conexoes
+from src.models.settings.db_connection_handler import db_connection_handler
 
 if __name__ == "__main__":
+    # Criar conexao com o servidor db antes de subir o server flask
+    db_connection_handler.connect()
+
     app.run(host="0.0.0.0", port=3000, debug=True)
 
---
+++ src/controllers/link_creator.py
@@ -0,0 +1,31 @@
+import uuid
+
+class LinkCreator:
+    def __init__(self, links_repository) -> None:
+        self.__links_repository = links_repository
+
+    def create(self, body, trip_id) -> dict:
+        try:
+            link_id = str(uuid.uuid4())
+            link_infos = {
+                "link": body["url"],
+                "title": body["title"],
+                "id": link_id,
+                "trip_id": trip_id,
+            }
+            self.__links_repository.create_link(link_infos)
+
+            return {
+                "body": {
+                    "linkId": link_id
+                },
+                "status_code": 201
+            }
+        except Exception as exception:
+            return {
+                "body": {
+                    "error": "Bad Request",
+                    "message": str(exception)
+                },
+                "status_code": 400
+            }
